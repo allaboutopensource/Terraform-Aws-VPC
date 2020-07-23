0e5b86147dbfec307fe9a8318c1fe41773e63587
Sunil kathait <5890851+allaboutopensource@users.noreply.github.com>
2020-07-23 14:46:23 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "access_key" {
+    default = "user-access-key"
+}
+
+variable "secret_key" {
+    default = "user-access-secret-key"
+}
+
+variable "region" {
+    default = "us-east-1"
+}
